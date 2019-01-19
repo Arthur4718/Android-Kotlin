bfa4e0d5f9d7788c528dd4b5ce3ef111c628e3fb
Unknown <arthur.gomes_4718@hotmail.com>
2019-01-18 23:09:59 -0200

    calculator layout

---
+++ aula1/app/src/main/java/madcode01/com/br/aula1/MainActivity.kt
@@ -0,0 +1,12 @@
+package madcode01.com.br.aula1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
