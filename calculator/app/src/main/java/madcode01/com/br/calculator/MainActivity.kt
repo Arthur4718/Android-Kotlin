76ed86246f2b604bc68d4eaf88e6f552b5b01842
Unknown <arthur.gomes_4718@hotmail.com>
2019-01-19 01:04:30 -0200

    late init example

--- calculator/app/src/main/java/madcode01/com/br/calculator/MainActivity.kt
+++ calculator/app/src/main/java/madcode01/com/br/calculator/MainActivity.kt
@@ -2,8 +2,15 @@ package madcode01.com.br.calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    //Late init - Its a kotlin resource to tell android that this element will be init later
+    private lateinit var result: EditText
+    private lateinit var newNumber: EditText
+    private val displayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
