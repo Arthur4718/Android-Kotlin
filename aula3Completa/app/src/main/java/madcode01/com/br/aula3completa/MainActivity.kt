941ddbd028b68cf47524d41259c9dba441bae517
Unknown <arthur.gomes_4718@hotmail.com>
2019-01-24 20:26:12 -0200

    Final da aula

--- aula3Completa/app/src/main/java/madcode01/com/br/aula3completa/MainActivity.kt
+++ aula3Completa/app/src/main/java/madcode01/com/br/aula3completa/MainActivity.kt
@@ -1,12 +1,33 @@
 package madcode01.com.br.aula3completa
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnIniciar.setOnClickListener {
+
+            val intentTelaIdade = Intent(applicationContext, TelaIdade::class.java)
+
+            intentTelaIdade.putExtra("TAG_NOME", edtNome.text.toString())
+
+            startActivity(intentTelaIdade)
+
+
+
+//            val intentIdade = Intent(applicationContext, TelaIdade::class.java).apply {
+//                putExtra("TAG_NOME", edtNome.text.toString())
+//            }
+//            startActivity(intentIdade)
+
+
+        }
+
     }
 }
--- kotlinextensions/app/src/main/java/madcode01/com/br/kotlin_extensions/MainActivity.kt
+++ kotlinextensions/app/src/main/java/madcode01/com/br/kotlin_extensions/MainActivity.kt
@@ -8,59 +8,30 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //setting up the array of text for the spinner
-//        ArrayAdapter.createFromResource(
-//            this,
-//            R.array.Moedas,
-//            android.R.layout.simple_spinner_item
-//        ).also { adapter ->
-//            // Specify the layout to use when the list of choices appears
-//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//            // Apply the adapter to the spinner
-//            spinnerCountry.adapter = adapter
-//        }
-
-        //An simple way of doing with an hardcoded array
-        val myCurrencies = arrayOf("BRL", "USD", "NAN")
-
-        spinnerCurrency.adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, myCurrencies)
+        val countryList = arrayOf("Brazil","Venezuela","Suriname","Guiana","Uruguai")
+        spnCountry.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, countryList)
 
 
-        spinnerCurrency.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        spnCountry.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onNothingSelected(parent: AdapterView<*>?) {
-
+                Toast.makeText( applicationContext ,"Nothing was selected", Toast.LENGTH_SHORT).show()
             }
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                Toast.makeText(applicationContext, "Item Selected", Toast.LENGTH_SHORT).show()
-
+                Toast.makeText( applicationContext ,"Item Selected!", Toast.LENGTH_SHORT).show()
             }
+
         }
-
-
-        val myCountries = arrayOf("Brazil", "Venezuela", "Uruguai")
-
-        spinnerCountry.adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, myCountries)
-
-        spinnerCountry.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                Toast.makeText(applicationContext, "Item Selected", Toast.LENGTH_SHORT).show()
-
-            }
-        }
-
-
 
 
     }
+
+
 }
