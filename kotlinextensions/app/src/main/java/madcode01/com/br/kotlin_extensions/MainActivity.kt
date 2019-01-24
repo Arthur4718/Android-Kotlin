ffd3fb9c752d611ad9f59654454e6bec0c6d7b52
Unknown <arthur.gomes_4718@hotmail.com>
2019-01-24 06:40:27 -0200

    spinner event

--- kotlinextensions/app/src/main/java/madcode01/com/br/kotlin_extensions/MainActivity.kt
+++ kotlinextensions/app/src/main/java/madcode01/com/br/kotlin_extensions/MainActivity.kt
@@ -15,23 +15,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //setting up the array of text for the spinner
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.Moedas,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            mySpinner.adapter = adapter
-        }
+//        ArrayAdapter.createFromResource(
+//            this,
+//            R.array.Moedas,
+//            android.R.layout.simple_spinner_item
+//        ).also { adapter ->
+//            // Specify the layout to use when the list of choices appears
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//            // Apply the adapter to the spinner
+//            spinnerCountry.adapter = adapter
+//        }
 
         //An simple way of doing with an hardcoded array
         val myCurrencies = arrayOf("BRL", "USD", "NAN")
 
         spinnerCurrency.adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, myCurrencies)
 
+
         spinnerCurrency.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(applicationContext, "Item Selected", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+
+        val myCountries = arrayOf("Brazil", "Venezuela", "Uruguai")
+
+        spinnerCountry.adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, myCountries)
+
+        spinnerCountry.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
             }
