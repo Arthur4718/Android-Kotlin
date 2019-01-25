9e01b5e4cf4786f3d9e7b62b655bc221c73125db
Unknown <arthur.gomes_4718@hotmail.com>
2019-01-25 18:13:13 -0200

    Layout tela 2

--- SharedPrefs/app/src/main/java/madcode01/com/br/sharedprefs/MainActivity.kt
+++ SharedPrefs/app/src/main/java/madcode01/com/br/sharedprefs/MainActivity.kt
@@ -1,12 +1,21 @@
 package madcode01.com.br.sharedprefs
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCadastrar.setOnClickListener {
+            //TODO checar se o usuário já não fez login anteriormente
+
+            val intentTelaCadastro = Intent(applicationContext, Tela_Cadastro::class.java)
+            startActivity(intentTelaCadastro)
+        }
     }
 }
--- SharedPrefs/app/src/main/java/madcode01/com/br/sharedprefs/Tela_Cadastro.kt
+++ SharedPrefs/app/src/main/java/madcode01/com/br/sharedprefs/Tela_Cadastro.kt
@@ -2,11 +2,68 @@ package madcode01.com.br.sharedprefs
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_tela__cadastro.*
 
 class Tela_Cadastro : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela__cadastro)
+
+        btnConcluirForm1.setOnClickListener {
+
+            enviarDados()
+        }
+
     }
+    private fun enviarDados(){
+
+        if(!validarNome()){
+            return
+        }
+        if(!validarEmail()){
+            return
+        }
+        if(!validarIdade()){
+            return
+        }
+
+        
+    }
+
+    private fun validarNome(): Boolean {
+        //Checar se os dados são validos!
+        if(edtNomeCompleto.text.isEmpty()){
+            inputLaytouNome.error = "Preencha o nome por favor"
+            return false
+        }
+        else{
+            inputLaytouNome.error = null
+        }
+
+        return true
+    }
+
+    private fun validarEmail(): Boolean{
+
+        if(edtEmail.text.isEmpty()){
+            inputLayoutEmail.error = "Preencha o e-mail por favor"
+            return false
+        }
+        else{
+            inputLayoutEmail.error = null
+        }
+        return true
+    }
+    private fun validarIdade() : Boolean{
+        if(edtIdade.text.isEmpty()){
+            inputLayoutIdade.error = "Preencha a idade por favor"
+            return false
+        }
+        else{
+            inputLayoutIdade.error = null
+        }
+        return true
+    }
+
 }
