30cb48f3d635cfce5e4beaaae7a2551fe2bbf533
Unknown <arthur.gomes_4718@hotmail.com>
2019-01-25 16:21:29 -0200

    Shared prefs aula 2

---
+++ SharedPrefs/app/src/main/java/madcode01/com/br/sharedprefs/Tela_Cadastro.kt
@@ -0,0 +1,12 @@
+package madcode01.com.br.sharedprefs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela_Cadastro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela__cadastro)
+    }
+}
