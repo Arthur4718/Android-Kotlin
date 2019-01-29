50582c2a12d84b901b4a5f8afd6255425de15ca2
Unknown <arthur.gomes_4718@hotmail.com>
2019-01-29 19:27:42 -0200

    Intent para câmera.

--- Intents/app/src/main/java/madcode01/com/br/intents/MainActivity.kt
+++ Intents/app/src/main/java/madcode01/com/br/intents/MainActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.provider.MediaStore
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,8 +17,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnAbrirCâmera.setOnClickListener {
+            abrirCamera()
+        }
+
         btnAbrirContatos.setOnClickListener {
             selectContact()
+        }
+
+        btnAbrirNovaTela.setOnClickListener{
+            abrirNovaTela()
         }
 
     }
@@ -29,6 +38,21 @@ class MainActivity : AppCompatActivity() {
         if (intent.resolveActivity(packageManager) != null) {
             startActivityForResult(intent, REQUEST_SELECT_CONTACT)
         }
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+     fun abrirCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    fun abrirNovaTela(){
+        val intentNovaTela = Intent(this, Tela2::class.java)
+        startActivity(intentNovaTela)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
