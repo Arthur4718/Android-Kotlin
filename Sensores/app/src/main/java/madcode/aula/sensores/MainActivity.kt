38d68a732469d6d8c4832645dfe3326e3bf7d785
Unknown <arthur.gomes_4718@hotmail.com>
2019-01-31 19:12:13 -0200

    Lista

--- Sensores/app/src/main/java/madcode/aula/sensores/MainActivity.kt
+++ Sensores/app/src/main/java/madcode/aula/sensores/MainActivity.kt
@@ -2,23 +2,24 @@ package madcode.aula.sensores
 
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var mSensorManager: SensorManager
-    
+    private var mGravity: Sensor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         listAllSenros()
-
 
     }
 
@@ -36,5 +37,27 @@ class MainActivity : AppCompatActivity(){
             Log.d("main", "Not Active")
         }
     }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mGravity?.also { light ->
+            mSensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mSensorManager.unregisterListener(this)
+    }
+
 
 }
